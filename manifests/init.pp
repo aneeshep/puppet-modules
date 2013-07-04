574185ad6cca6199d2913088969acbabe64528a9
aneeshep <me@aneesh.info>
2013-07-04 15:49:32 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,5 +37,10 @@
 #
 class tomcat {
 
+  archive { 'apache-tomcat-6.0.26':
+    ensure => present,
+    url    => 'http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.26/bin/apache-tomcat-6.0.26.tar.gz',
+    target => '/opt',
+  }
 
 }
